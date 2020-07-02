d1eaff58a6355cf5be3de8924a1a2d2c20ff52b7
Ivan315ui <ivanemanuel315@gmail.com>
2020-07-02 18:34:21 -0300

    slider funcional(con js esta vez), pagina de inicio y nombre de los archivos de estilos, cambiados

---
+++ js/demo.js
@@ -0,0 +1,57 @@
+// where should the slider start at
+var index = 0;
+// dashes that indicate the position
+var indicator = document.getElementsByClassName("indicator");
+// add the onhover behavior (does not work via css because js main function overrides it)
+for (let i = 0; i < indicator.length; i++) {
+    indicator[i].onmouseover = () => {
+        indicator[i].style.background = "black";
+    }
+    indicator[i].onmouseout = () => {
+        indicator[i].style.background = "#0F5F75";
+        if (i == index) {
+            indicator[i].style.background = "black";
+        }
+    }
+}
+// start and repeat slider's main function every 4.5 seconds
+currentSlide(index);
+setInterval(() => {
+    index++;
+    if (index < 0) {
+        index = 3;
+    }
+    if (index > 3) {
+        index = 0;
+    }
+    currentSlide(index)
+}, 4500);
+// right arrow function
+function nextSlide() {
+    index++;
+    if (index > 3) {
+        index = 0;
+    }
+    currentSlide(index);
+}
+// left arrow function 
+function previousSlide() {
+    index--;
+    if (index < 0) {
+        index = 3;
+    }
+    currentSlide(index);
+}
+// slider's main function
+function currentSlide(value) {
+    index = value;
+    var wrapper = document.getElementById("slide-wrapper");
+    var margin = "-" + value + "00%";
+    wrapper.style.marginLeft = margin;
+    for (let i = 0; i < indicator.length; i++) {
+        indicator[i].style.background = "#0F5F75";
+        if (i == value) {
+            indicator[i].style.background = "black";
+        }
+    }
+}
