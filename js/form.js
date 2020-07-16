d2ce56c8d03ead724b548adf85a70657acae0e1a
Ivan315ui <ivanemanuel315@gmail.com>
2020-07-16 14:26:04 -0300

    pagina de registro e inicio terminada

--- js/form.js
+++ js/form.js
@@ -1,17 +1,164 @@
+//TODO Password visibility
+
+// todo change content
+
+// window.location.search devuelve los parámetros de la url
+var url = window.location.search;
+var inputs = document.getElementsByClassName("inputs");
+var registerLink = document.getElementById('register');
+var loginLink = document.getElementById('login');
+var optionLine = document.getElementsByClassName("underline")[0];
+var currentState = "";
+// form validation variables
+var errorElement = "";
 // current element
 var element;
 // current format
 var formato;
 // form element
 const form = document.getElementById('form');
-// error paragraphs
-const errorElement = document.getElementsByClassName('error');
+// function called on load to check form's initial display
+function getFormtoDisplay() {
+	// if the current url has the following query, means it came from "ingresar" option
+	if (url === "?mode=register") {
+		// so, hide the signup form
+		inputs[0].style.display = "none";
+		changeContent("register");
+	} else {
+		// let it be login by default
+		inputs[1].style.display = "none";
+		changeContent("login");
+	}
+}
 
-// to catch errors before submitting, add an eventlistener on form when submit
-form.addEventListener('submit', (e) => {
+getFormtoDisplay();
+
+// TODO force correct form's initial state
+function changeContent(state) {
+	if (state == "register") {
+		// if register mode is already set
+		if (currentState === "register") {
+			optionLine.style.transition = "0.2s linear";
+			optionLine.style.height = registerLink.parentElement.offsetHeight + "px";
+			optionLine.style.backgroundColor = "#FFA536";
+			setTimeout(() => {
+				optionLine.style.height = "2px";
+				optionLine.style.marginBottom = registerLink.parentElement.offsetHeight - 2 + "px";
+			}, 200);
+			setTimeout(() => {
+				optionLine.style.height = registerLink.parentElement.offsetHeight + "px";
+				optionLine.style.marginBottom = 0;
+			}, 400);
+			setTimeout(() => {
+				optionLine.style.height = "2px";
+			}, 600);
+			setTimeout(() => {
+				optionLine.style.backgroundColor = "#0F5F75";
+			}, 800);
+		} else {
+			// if not, set it to hide, and display the other one
+			inputs[0].style.display = "none";
+			inputs[1].style.display = "flex";
+			optionLine.style.width = registerLink.offsetWidth + "px";
+			optionLine.style.transition = "0.5s ease-in-out";
+			optionLine.style.marginLeft = parseInt(registerLink.parentElement.offsetWidth) - parseInt(optionLine.style.width) + "px";
+			currentState = "register";
+			catchErrors(currentState);
+			for (let i = 0; i < errorElement.length; i++) {
+				errorElement[i].innerText = "";
+			}
+			document.getElementById('greeting').innerText = "¡Creá tu cuenta!";
+		}
+	} else {
+		if (currentState === "login") {
+			optionLine.style.transition = "0.2s linear";
+			optionLine.style.height = registerLink.parentElement.offsetHeight + "px";
+			optionLine.style.backgroundColor = "#FFA536";
+			setTimeout(() => {
+				optionLine.style.height = "2px";
+				optionLine.style.marginBottom = registerLink.parentElement.offsetHeight - 2 + "px";
+			}, 200);
+			setTimeout(() => {
+				optionLine.style.height = registerLink.parentElement.offsetHeight + "px";
+				optionLine.style.marginBottom = 0;
+			}, 400);
+			setTimeout(() => {
+				optionLine.style.height = "2px";
+			}, 600);
+			setTimeout(() => {
+				optionLine.style.backgroundColor = "#0F5F75";
+			}, 800);
+		} else {
+			inputs[1].style.display = "none";
+			inputs[0].style.display = "flex";
+			optionLine.style.width = loginLink.offsetWidth + "px";
+			optionLine.style.transition = "0.5s ease-in-out";
+			optionLine.style.marginLeft = 0;
+			currentState = "login";
+			catchErrors(currentState);
+			for (let i = 0; i < errorElement.length; i++) {
+				errorElement[i].innerText = "";
+			}
+			document.getElementById('greeting').innerText = "¡Bienvenido de nuevo!";
+		}
+	}
+}
+
+registerLink.onclick = () => {
+	changeContent("register");
+}
+
+loginLink.onclick = () => {
+	changeContent("login");
+}
+
+var loginReferenceFunction = (e) => {
 	// error messages
 	let messages = [];
-	element = document.getElementById('nombre');
+	// error paragraphs
+	errorElement = document.getElementsByClassName('login-error');
+	element = document.getElementById('login-mail');
+	formato = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if (element.value === '' || element.value == null) {
+		messages.push("No se proveyó un Email.");
+	} else if (!element.value.match(formato)) {
+		messages.push("Revisa el formato del campo Email, un formato apropiado es similar al siguiente: \"ejemplo@mail.com\".");
+	} else {
+		messages.push("");
+	}
+	element = document.getElementById('login-pass');
+	formato = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
+	if (element.value === '' || element.value == null) {
+		messages.push("No se proveyó una contraseña.");
+	} else if (!element.value.match(formato)) {
+		messages.push("Se requiere al menos una minúscula, una mayúscula y un número para la contraseña.");
+	} else if (element.value.length < 8 || element.value.length > 20) {
+		messages.push("La contraseña debe tener entre 8 y 20 caracteres");
+	} else {
+		messages.push("");
+	}
+	for (let i = 0; i < messages.length; i++) {
+		if (messages[i] !== "") {
+			e.preventDefault();
+			for (let i = 0; i < errorElement.length; i++) {
+				errorElement[i].innerText = messages[i];
+				errorElement[i].style.padding = "0 5px";
+			}
+			setTimeout(() => {
+				element = document.getElementById('login-mail');
+				element.focus();
+			}, 1000);
+			break;
+		}
+	}
+};
+
+var registerReferenceFunction = (e) => {
+	// error messages
+	let messages = [];
+	// error paragraphs
+	errorElement = document.getElementsByClassName('error');
+	element = document.getElementById('name');
 	formato = /[A-Za-záéíóú]+$/i;
 	if (element.value === '' || element.value == null) {
 		messages.push("No se proveyó un nombre.");
@@ -23,7 +170,7 @@ form.addEventListener('submit', (e) => {
 	} else {
 		messages.push("");
 	}
-	element = document.getElementById('apellido');
+	element = document.getElementById('surname');
 	if (element.value === '' || element.value == null) {
 		messages.push("No se proveyó un apellido.");
 	} else if (!element.value.match(formato)) {
@@ -73,23 +220,26 @@ form.addEventListener('submit', (e) => {
 				errorElement[i].style.padding = "0 5px";
 			}
 			setTimeout(() => {
-				element = document.getElementById('nombre');
+				element = document.getElementById('name');
 				element.focus();
 			}, 1000);
 			break;
 		}
 	}
-});
+};
 
-//TODO Password visibility
-
-// assign onclick actions to option 
-function assignActions() {
-	var inicio = document.getElementById('iniciar');
-	var registro = document.getElementById('registro');
-	inicio.addEventListener('click', toggleSession('login'));
-	registro.addEventListener('click', toggleSession('register'));
+function catchErrors(state) {
+	if (state === "register") {
+		// remove previous eventListener
+		form.removeEventListener('submit', loginReferenceFunction);
+		// add error handler for register form
+		form.addEventListener('submit', registerReferenceFunction);
+	} else if (state === "login") {
+		//remove previous eventListener
+		form.removeEventListener('submit', registerReferenceFunction);
+		// add handler for login
+		form.addEventListener('submit', loginReferenceFunction);
+	}
 }
-//TODO Change content
 
-// TODO force correct form's initial state
+catchErrors(currentState);
