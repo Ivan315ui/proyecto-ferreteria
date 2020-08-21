cdeca410bd72cdafccbb2b4e2ec73c3802dca91d
jereet <55718356+jereet@users.noreply.github.com>
2020-08-21 00:57:44 -0300

    Add files via upload

--- templates/registro.php
+++ templates/registro.php
@@ -12,7 +12,7 @@ if (isset($_POST['register'])){
         $consulta = "INSERT INTO usuarios (Email, Nombre, Apellido, Contraseña, DNI) VALUES ('$mail','$name','$surname','$pass','$dni')";
         $resultado = mysqli_query($conexion, $consulta);
         if ($resultado) {
-            header("Location:registro-exito.html");
+            header("Location: registro-exito.html");
         }else{
             header("Location: registro-error.php");
         }
